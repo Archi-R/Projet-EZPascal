e229d26a79c59d41b01b0853122eaf26d1d9bddc
Archi <archib71@gmail.com>
2021-07-24 20:37:18 +0200

    writing
    implémentation de la classe Writing, qui va écrire les parties du fichier
    implémentation du couton générer qui va appeler correctement les méthodes de Writing

--- Projet_EZPascal_Csharp/Form1.cs
+++ Projet_EZPascal_Csharp/Form1.cs
@@ -33,25 +33,21 @@ namespace Projet_EZPascal_Csharp
         {
 
         }
-
         private void label2_Click(object sender, EventArgs e)           //exercise indicator
         {
 
         }
-
         private void B_next_Click(object sender, EventArgs e)           //next exercise
         {
             this.exSelector++;
             refresh();
         }
-
+       
         private void B_previous_Click(object sender, EventArgs e)       //previous exercise
         {
             this.exSelector--;
             refresh();
         }
-
-             
 
         private void button2_Click(object sender, EventArgs e)          // ??
         {
@@ -62,7 +58,6 @@ namespace Projet_EZPascal_Csharp
         {
             refresh();
         }
-
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)//number of sub-exercises
         {
@@ -105,7 +100,29 @@ namespace Projet_EZPascal_Csharp
 
         private void B_generate_Click(object sender, EventArgs e)       //generate the file
         {
-            
+            Writing wr = new Writing(this.name,"PATH");
+
+            wr.entete(nb_ex);
+            //ecriture de l'entete du programme
+
+            //boucle de création des exercices
+            for (int j = 1; j <= nb_ex; j++) {
+                wr.numex_debut(j);          //écriture des numEx
+
+                if (array_subExs[j] != 0) {
+                    wr.entete_SN();           //écriture de l'entête des sous num
+
+                    for (int k=1;k<= array_subExs[j];k++)
+                    {
+                         wr.SN(k);            //écriture des sous num
+                    }
+
+                    wr.SN_fin();              //écriture des fins des sousNum
+                }
+                wr.numex_fin(j);
+            }
+
+            wr.FIN(nb_ex);                   //écriture de la fin du programme
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
---
+++ Projet_EZPascal_Csharp/Writing.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+
+namespace Projet_EZPascal_Csharp
+{
+    internal class Writing
+    {
+
+        private string name;
+        private string path;
+
+        public Writing(string name, string path)
+        {
+            this.name = name ?? throw new ArgumentNullException(nameof(name));
+            this.path = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        public void entete(int nbEx)   //file heading (headings comments, name, "uses", var etc...)
+        { //var fichier:text; nom:string;nbEx:Integer
+
+            StreamWriter sw = new StreamWriter(path);    //open the file
+            
+            //Write the heading of the file
+            sw.WriteLine("program ", name, ";        //nom de votre programme");
+            sw.WriteLine("{");
+            sw.WriteLine("Interface générique simple à remplir seulement entre les begin...end pour faire une suite d'exercices");
+            sw.WriteLine("}");
+            sw.WriteLine("{");
+            sw.WriteLine("Programme :");
+            sw.WriteLine("Rôle :");
+            sw.WriteLine("Principe :");
+            sw.WriteLine("Auteur :");
+            sw.WriteLine("}");
+            sw.WriteLine("");
+            sw.WriteLine("//uses                              //vos unités     (décommenter pour remplir)");
+            sw.WriteLine("");
+            sw.WriteLine("//type                              //vos types");
+            sw.WriteLine("");
+            sw.WriteLine("var                                 //vos variables   /!\\Ne pas toucher à celles déjà présentes");
+            sw.WriteLine("  numeroEx,sousNum:Integer;");
+            sw.WriteLine("");
+            sw.WriteLine("");
+            sw.WriteLine("Begin ");
+            sw.WriteLine("writeln(''Tapez le numero de votre exercice que vous voulez executer(de 1 a "+nbEx+ "):'');");
+            sw.WriteLine("writeln(''Ou tapez 0 si vous voulez sortir''); ");
+            sw.WriteLine("readln(numeroEx);");
+            sw.WriteLine("");
+            sw.WriteLine("while numeroEx <> 0 do");
+            sw.WriteLine("  begin");
+            sw.WriteLine("  case numeroEx of");
+
+            sw.Close();         //close the file
+        }                       
+
+        public void numex_debut(int j)              //begginning of exercises : the "begin", its number
+        {//(j:integer; var fichier:text;nom:string)
+            StreamWriter sw = new StreamWriter(path);   //open the file
+            
+            sw.WriteLine("       "+j+":");
+            sw.WriteLine("         begin");
+            sw.WriteLine("");
+            
+            sw.Close();                                 //close the file
+        }
+
+        public void entete_SN()                     //heading of sub-exs : thr "case of..."
+        {//(var fichier:text; nom:string)
+            StreamWriter sw = new StreamWriter(path);   //open the file
+
+            sw.WriteLine("         Writeln('Quel sous-numero ?');");
+            sw.WriteLine("         readln(sousNum);");
+            sw.WriteLine("         case sousNum of");
+
+            sw.Close();                                 //close the file
+        }
+
+        public void SN(int k)                       //the sub exs their "begin", numbers, and "end"
+        {//(k:integer; var fichier:text;nom:string)
+            StreamWriter sw = new StreamWriter(path);   //open the file
+
+            sw.WriteLine("         ', k, ':");
+            sw.WriteLine("           begin");
+            sw.WriteLine("");
+            sw.WriteLine("           end;                  //end de fin de sousNum "+k);
+            sw.WriteLine("");
+            
+            sw.Close();                                 //close the file
+        }
+
+        public void SN_fin()                        //end of sub ex's "case of"
+        {//(var fichier:text; nom:string)
+            StreamWriter sw = new StreamWriter(path);   //open the file
+
+            sw.WriteLine("         end;                       //end de fin de Case sousNum of");
+
+            sw.Close();                                 //close the file
+        }
+
+        public void numex_fin(int j)                //end of exerices "case of"
+        {//(j:integer; var fichier:text;nom:string)
+            StreamWriter sw = new StreamWriter(path);   //open the file
+
+            sw.WriteLine("         end;                       //end de fin de numEx "+j);
+
+            sw.Close();                                 //close the file
+        }
+
+        public void FIN(int nbEx)                   //file footer : option "incorrect number", asking for new number and end of loops
+        {//(var fichier:text; nom:string;nbEx:Integer)
+            StreamWriter sw = new StreamWriter(path);   //open the file
+
+            sw.WriteLine("else");
+            sw.WriteLine("  begin");
+            sw.WriteLine("  Writeln(''Numero incorrect'');");
+            sw.WriteLine("  end;                              //end de fin de else");
+            sw.WriteLine("");
+            sw.WriteLine("end;                                //end de fin de Case numeroEx of ");
+            sw.WriteLine("");
+            sw.WriteLine("writeln(''Tapez le numero de votre exercice que vous voulez executer (de 1 a ', nbEx, '):'');");
+            sw.WriteLine("writeln(''Ou tapez 0 si vous voulez sortir'');");
+            sw.WriteLine("readln(numeroEx);");
+            sw.WriteLine("");
+            sw.WriteLine("end;                               //end de fin de while numeroEx <> 0");
+            sw.WriteLine("");
+            sw.WriteLine("end.                               //end de fin de programme                       ~~Auteur du pré code généré : Ronan PEYREL, promo 2020");
+
+            sw.Close();                                 //close the file
+        }
+
+    }
+}
